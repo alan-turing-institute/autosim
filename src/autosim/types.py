d128555bbbce79b1eafc9448473e563f0315a413
Edwin Brown <w.edwin.brown@outlook.com>
2026-02-20 14:51:04 +0000

    Add all typing and val mix in from autoemulate

--- src/autosim/types.py
+++ src/autosim/types.py
@@ -1,10 +1,21 @@
-from typing import TypeAlias
+from typing import Any, TypeAlias
 
 import numpy as np
 import torch
 import torch.utils
 import torch.utils.data
+from torch.utils.data import DataLoader
 
 NumpyLike: TypeAlias = np.ndarray
 TensorLike: TypeAlias = torch.Tensor
+DistributionLike: TypeAlias = torch.distributions.Distribution
+InputLike: TypeAlias = NumpyLike | TensorLike | DataLoader | torch.utils.data.Dataset
+OutputLike: TypeAlias = DistributionLike | TensorLike
+ParamLike: TypeAlias = Any
+TuneParams: TypeAlias = dict[str, list[ParamLike]]
+ModelParams: TypeAlias = dict[str, ParamLike]
+TransformedEmulatorParams: TypeAlias = dict[str, ParamLike]
 DeviceLike: TypeAlias = str | torch.device
+
+# Torch dtype's
+TorchScalarDType = (torch.float32, torch.float64, torch.int32, torch.int64)
