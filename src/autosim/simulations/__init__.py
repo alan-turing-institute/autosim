4e8ed8e1be8ca11ec816559952816072b6a315b3
Sam Greenbury <sgreenbury@turing.ac.uk>
2026-02-25 14:36:04 +0000

    Move simulations to experimental

--- src/autosim/simulations/__init__.py
+++ src/autosim/simulations/__init__.py
@@ -1,11 +1,8 @@
 from .advection_diffusion import AdvectionDiffusion
 from .advection_diffusion_multichannel import AdvectionDiffusionMultichannel
-from .compressible_fluid import CompressibleFluid2D
 from .epidemic import Epidemic
 from .flow_problem import FlowProblem
 from .gray_scott import GrayScott
-from .hydrodynamics_2d import Hydrodynamics2D
-from .lattice_boltzmann import LatticeBoltzmann
 from .projectile import Projectile, ProjectileMultioutput
 from .reaction_diffusion import ReactionDiffusion
 from .seir import SEIRSimulator
@@ -14,9 +11,6 @@ ALL_SIMULATORS = [
     ReactionDiffusion,
     AdvectionDiffusion,
     AdvectionDiffusionMultichannel,
-    CompressibleFluid2D,
-    Hydrodynamics2D,
-    LatticeBoltzmann,
     Epidemic,
     SEIRSimulator,
     FlowProblem,
@@ -28,12 +22,9 @@ ALL_SIMULATORS = [
 __all__ = [
     "AdvectionDiffusion",
     "AdvectionDiffusionMultichannel",
-    "CompressibleFluid2D",
     "Epidemic",
     "FlowProblem",
     "GrayScott",
-    "Hydrodynamics2D",
-    "LatticeBoltzmann",
     "Projectile",
     "ProjectileMultioutput",
     "ReactionDiffusion",
