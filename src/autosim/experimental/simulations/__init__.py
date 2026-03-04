6019fd730e88e3fe9acd1fbbf25bbfc517bdc86b
Sam Greenbury <sgreenbury@turing.ac.uk>
2026-03-04 09:45:44 +0000

    Add Gross-Pitaevskii simulator and example

--- src/autosim/experimental/simulations/__init__.py
+++ src/autosim/experimental/simulations/__init__.py
@@ -1,5 +1,6 @@
 from .compressible_fluid import CompressibleFluid2D
 from .gray_scott import GrayScott
+from .gross_pitaevskii import GrossPitaevskiiEquation2D
 from .hydrodynamics_2d import Hydrodynamics2D
 from .lattice_boltzmann import LatticeBoltzmann
 from .navier_stokes_conditioned import ConditionedNavierStokes2D
@@ -16,6 +17,7 @@ __all__ = [
     "CompressibleFluid2D",
     "ConditionedNavierStokes2D",
     "GrayScott",
+    "GrossPitaevskiiEquation2D",
     "Hydrodynamics2D",
     "LatticeBoltzmann",
 ]
